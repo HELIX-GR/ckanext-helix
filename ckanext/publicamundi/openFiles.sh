ce1034d86dd7cc7749ee90a668c6295fd578de84
Nikiforos <nikleonidakis@gmail.com>
2018-06-11 16:45:12 +0300

    Minor fixes

--- ckanext/publicamundi/openFiles.sh
+++ ckanext/publicamundi/openFiles.sh
@@ -7,10 +7,10 @@ sudo gedit lib/metadata/base.py &
 sudo gedit lib/metadata/validators.py &
 sudo gedit templates/package/snippets/package_basic_fields.html &
 sudo gedit templates/package/snippets/package_metadata_fields.html &
-sudo gedit /usr/lib/ckan/default/src/extensions/ckanext-userdatasets/ckanext/userdatasets/logic/action/update.py &
+sudo gedit /usr/lib/ckan-2.8/default/src/extensions/ckanext-userdatasets/ckanext/userdatasets/logic/action/update.py &
 #sudo gedit /usr/lib/ckan/default/src/extensions/ckanext
 sudo gedit plugins.py &
-sudo gedit /usr/lib/ckan/default/src/extensions/ckanext-helixtheme/ckanext/helixtheme/public/css/main.css &
+sudo gedit /usr/lib/ckan-2.8/default/src/extensions/ckanext-helixtheme/ckanext/helixtheme/public/css/main.css &
 
 
 
