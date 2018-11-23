8dcf0bd2d8c6d18635bf66a279f4b9bbe7ae7b01
nleond <nikleonidakis@gmail.com>
2018-11-23 20:59:19 +0200

    Translations

--- ckanext/helix/public/js/modules/breadcrumb-overflow.js
+++ ckanext/helix/public/js/modules/breadcrumb-overflow.js
@@ -15,21 +15,20 @@ this.ckan.module('breadcrumb-overflow', function ($, _) {
             var $el = this.el            //Breadcrumbs auto hide all but last element
             var bread_items = $('.breadcrumbs-part:first').nextAll();
             bread_items = bread_items.not(':last');
-            console.log(bread_items, "Hello, world!");
             bread_items.each(function () {
                 //console.log($(this).context.innerText);
-                //$(this).context.innerText = "...";
+                //$(this).innerHTML = "...";
                 console.log($(this), "Hello, world2");
                 $(this).addClass('hide-overflow');
             });
             
             var first_breadcrumb = $('.breadcrumbs-part:first');
-            var txt3 = document.createElement("a");   // Create with DOM
-            txt3.className = "breadcrumbs-part";
-            txt3.innerHTML = " ...";
-            if (bread_items>1){
-                first_breadcrumb.after(txt3);
-            }
+            var txt = document.createElement("a");   // Create with DOM
+            var dots = document.createTextNode("...");
+            txt.appendChild(dots);
+            txt.className = "breadcrumbs-part";
+            first_breadcrumb.after(txt);
+            
             
 
             var toolbar = $('.breadcrumbs');
@@ -40,7 +39,7 @@ this.ckan.module('breadcrumb-overflow', function ($, _) {
                     $(this).removeClass('hide-overflow');
                     //addClass('breadcrumb-hide-text');
                 });
-                txt3.className = "breadcrumbs-part hide-overflow";
+                txt.className = "breadcrumbs-part hide-overflow";
             });
 
             toolbar.on('mouseleave', function () {
@@ -50,7 +49,7 @@ this.ckan.module('breadcrumb-overflow', function ($, _) {
                     $(this).addClass('hide-overflow');
                     //addClass('breadcrumb-hide-text');
                 });
-                txt3.className = "breadcrumbs-part";
+                txt.className = "breadcrumbs-part";
             });
 
 
